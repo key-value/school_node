d29f589da6f4fc19ef57442fc88952006fb8a9ce
wuyou <wuyou@mikeo2o.com>
2020-05-08 15:15:17 +0800

    更新年级样式

--- src/views/teaching/Glass.vue
+++ src/views/teaching/Glass.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <el-table :v-model="tableData">
-      <el-table-column lable="序号"></el-table-column>
-      <el-table-column label="班级"></el-table-column>
-      <el-table-column label="入学年级"></el-table-column>
+    <el-table :data="glassData">
+      <el-table-column width="100" prop="Id" label="序号"></el-table-column>
+      <el-table-column prop="Name" label="班级"></el-table-column>
+      <el-table-column prop="Grade" label="入学年级1"></el-table-column>
       <el-table-column label="当前学期"></el-table-column>
     </el-table>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped></style>
 
 <script lang="ts">
 import { Component, Vue, Provide } from 'vue-property-decorator';
 @Component({})
 export default class Glass extends Vue {
-  @Provide() tableData: any; // 门店选择框的展示标志
+  public glassData: any = [];
+
+  async created() {
+    console.log(`created`);
+    this.glassData = JSON.parse(`[{"Id":1,"Name":"1","Grade":5}]`);
+  }
 }
 </script>
